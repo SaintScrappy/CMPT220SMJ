e661e5250493e98ea6c7da177467d10fc454e57c
Bob John <Bob John>
2017-01-24 18:12:02 -0500

    Only begun problem 2.5, but conquered 2.6. Type in any integer, and get your sum.

---
+++ labs/lab1/FinanceMgmt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FinanceMgmt {
+  public static void main(String[] args) {
+    subtotal, gratuity rate;
+    System.out.print("Put your subtotal and gratuity rate. Please space between your numbers. (:");
+    celsius = sc.nextDouble();
+    fahrenheit = 32+(celsius * 9/5);
+    System.out.println(celsius +" C = " + fahrenheit + " F");
+     }
+}
---
+++ labs/lab1/SumAnInteger.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumAnInteger {
+  public static void main(String[] args) {
+    System.out.print("Enter an integer between 0 and 1000.");
+	java.util.Scanner input = new java.util.Scanner(System.in);
+	
+    int number = input.nextInt();
+
+    int lastDigit = number % 10;
+    int remainingNumber = number/10;
+    int secondLastDigit = remainingNumber % 10;
+    remainingNumber = remainingNumber/10;
+    int thirdLastDigit = remainingNumber % 10;
+    int sum = lastDigit + secondLastDigit + thirdLastDigit;
+    System.out.println("The sum of the integer" + " is " + sum);
+     }
+}
