1a0c46f7f6394dba0ff843481444e1c225bb8902
Bob John <Bob John>
2017-01-24 22:25:36 -0500

    Set up program's variable's.

--- labs/lab1/FinanceMgmt.java
+++ labs/lab1/FinanceMgmt.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class FinanceMgmt {
   public static void main(String[] args) {
-    subtotal, gratuity rate;
+    double subtotal;
+	double gratuityrate;
+	double gratuity; 
+	double total;
     System.out.print("Put your subtotal and gratuity rate. Please space between your numbers. (:");
-    celsius = sc.nextDouble();
-    fahrenheit = 32+(celsius * 9/5);
-    System.out.println(celsius +" C = " + fahrenheit + " F");
+   
+    
+    System.out.println( );
      }
 }
