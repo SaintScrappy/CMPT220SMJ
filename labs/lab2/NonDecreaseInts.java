ac70363ecf0cc696620748e06a9bbc1858806188
Bob John <Bob John>
2017-02-09 22:30:15 -0500

    Set up base for integer ordering program.

---
+++ labs/lab2/NonDecreaseInts.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NonDecreaseInts {
+  public static void main (String args []) {
+  
+  System.out.println("Enter three integers, 1 to 100 only.");
+  Scanner input = new Scanner(System.in);
+  
+  int n = input.nextInt();
+  System.out.println ("Your result is ");
+  }
+}
