608a173abc633e6f9993b064ad8f04fa8503640d
Bob John <Bob John>
2017-02-13 17:03:23 -0500

    Program displays user input from least to greater, going from top to bottom

--- labs/lab2/NonDecreaseInts.java
+++ labs/lab2/NonDecreaseInts.java
@@ -2,11 +2,39 @@ import java.util.Scanner;
 
 public class NonDecreaseInts {
   public static void main (String args []) {
+	  
+    Scanner input = new Scanner(System.in);
   
-  System.out.println("Enter three integers, 1 to 100 only.");
-  Scanner input = new Scanner(System.in);
-  
-  int n = input.nextInt();
-  System.out.println ("Your result is ");
+    System.out.print("Enter three integers:");
+      int number1 = input.nextInt();
+      int number2 = input.nextInt();
+      int number3 = input.nextInt();
+      int temp = 0;
+ 
+    if (number1 > number2) {
+     temp = number1;
+     number1 = number2;
+     number2 = temp;
   }
+ 
+    if (number2 > number3) {
+     temp = number2;
+     number2 = number3;
+     number3 = temp;
+  }
+ 
+    if (number1 > number2) {
+     temp = number1;
+     number1 = number2;
+     number2 = temp;
+  }
+ 
+  System.out.println(number1);
+  System.out.println(number2);
+  System.out.println(number3);
+ 
+ 
+ }
+ 
+
 }
