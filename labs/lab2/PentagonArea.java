79196868c94b6693997cf1dc7f72ef6594c0b52e
Bob John <Bob John>
2017-02-15 15:57:41 -0500

    Successfully takes user input regarding the measurements of a pentagon's center to calculate the area with the formula.

---
+++ labs/lab2/PentagonArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PentagonArea {
+  public static void main (String[] args){
+  
+  Scanner input = new Scanner(System.in);
+  System.out.print("Type in the length of the center to the vertex:");
+ 
+  double r = input.nextDouble();
+  double s = 2 * r * Math.sin(Math.PI / 5);
+  double area = 5 * Math.pow(s, 2) / (4 * Math.tan(Math.PI / 5));
+ 
+  area = Math.round(area*100) / 100.0;
+  System.out.println("The pentagon's area is " + area);
+ }
+}
