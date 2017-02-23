328093eb3e52de84793603484d73556fd0cc00fe
Bob John <Bob John>
2017-02-23 14:48:14 -0500

    Created two contrasting programs to compute the value of n from its powers.

---
+++ labs/lab3/SmallIntN.java
@@ -0,0 +1,11 @@
+public class SmallIntN {
+  public static void main(String [] args) {
+   
+  int n = 1;
+  while (Math.pow(n, 2)<=12000) {
+   n++;
+  }
+  System.out.println("The n is " + n);
+  System.out.println("The n^2 is " + Math.pow(n, 2));
+  }
+}
