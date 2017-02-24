35b0ac6543230c3447a2dd822e3e0a5690a18a54
Bob John <Bob John>
2017-02-24 13:49:44 -0500

    Added headings to programs.

--- labs/lab1/Problem13.java
+++ labs/lab1/Problem13.java
@@ -1,3 +1,10 @@
+//file: Problem13.java
+//author: Shawn M Joseph
+//course: CMPT 220
+//assignment: lab 1
+//Due January 26th
+//version: 1.0
+
 public class Problem13 {
   public static void main(String[] args) {
     System.out.println ("    J     A    V     V   A");
