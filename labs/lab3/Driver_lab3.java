1bd97afdd0897d75073e5008e7c09682ab1c8bda
Bob John <Bob John>
2017-03-09 23:43:52 -0500

    Minor updates on lab 3 programs.

--- labs/lab3/Driver_lab3.java
+++ labs/lab3/Driver_lab3.java
@@ -9,6 +9,18 @@ import java.util.Scanner;
 
 public class Driver_lab3 {
   public static void main (String [] args) {
-  
+	  
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+	double x = input.nextLong();
+	
+    double x;
+	double x1;
+	double y;
+	double y1;
+	double p;
+	
+	double formula = (x1−x2|p+|y1−y2|p)1/p
+	
   }
 }
