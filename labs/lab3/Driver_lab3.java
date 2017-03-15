55039c7618efeb0f304a7e44cf8ad9574af3ecab
Bob John <Bob John>
2017-03-14 23:33:25 -0400

    Solved Kattis DiffDistance problem, main issue was printing a message asking for user input.

--- labs/lab3/Driver_lab3.java
+++ labs/lab3/Driver_lab3.java
@@ -11,16 +11,20 @@ public class Driver_lab3 {
   public static void main (String [] args) {
 	  
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
 	
-    double x1 = input.nextDouble();
-	double y1 = input.nextDouble();
-	double x2 = input.nextDouble();
-	double y2 = input.nextDouble();
-	float p = input.nextFloat();
+	while (true) {
+      double x1 = input.nextDouble();
+	    if (x1 == 0)
+          break;
+	  double y1 = input.nextDouble();
+	  double x2 = input.nextDouble();
+	  double y2 = input.nextDouble();
+	  double p = input.nextDouble();
 	 
-	double pNorm = Math.pow((Math.pow((Math.abs(x1 - x2)),p)) + (Math.pow((Math.abs(y1 - y2)),p)),1/p);
-	System.out.print(pNorm);
+	System.out.printf("%.4f", Math.pow(Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2),p), 1/p));
+	System.out.println();
+	}
+	input.close();
   }
 }
 	//    (   (x1-x2)^p    +    (y1-y2)^p  )^1.0/p
