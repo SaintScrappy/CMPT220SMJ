51f2a1c2c2d7d4e7446ccbb1f7c58a3da29b1379
Bob John <Bob John>
2017-03-18 00:06:40 -0400

    Set up the base of program for convoluting two vector arrays.

---
+++ projects/1/VectConvolution.java
@@ -0,0 +1,24 @@
+//file: VectConvolution.java
+//author: Shawn M Joseph
+//course: CMPT 220
+//assignment: Project 1
+//Due March 30th
+//version: 1.0
+
+import java.util.Scanner;
+
+public class VectConvolution {
+  public static void main (String [] args) {
+	  
+    Scanner input = new Scanner(System.in);
+    double [] Vector1 = input.nextDouble();
+	double [] Vector2 = input.nextDouble();
+	
+	}
+ public static double[] convolveVectors(double[] Vector1, double[] Vector2) {
+   double[] Result = new double[(vFirst.length + vSecond.length) - 1];
+    
+
+    return Result;  
+  }
+}
