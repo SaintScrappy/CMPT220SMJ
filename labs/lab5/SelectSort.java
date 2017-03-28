c04d5b4353d207694963128c0c3710e662f73a34
Bob John <Bob John>
2017-03-28 18:31:47 -0400

    Began development of lab5

---
+++ labs/lab5/SelectSort.java
@@ -0,0 +1,14 @@
+//file: SelectSort.java
+//author: Shawn M Joseph
+//course: CMPT 220
+//assignment: lab 5
+//Due March 30th
+//version: 1.0
+
+public class SelectSort {
+  public static void main (String[] args) {
+	  
+    double [] select = {10.0, 5.0, 6.0, 25.0, 7.0, 8.0, 9.0, 10.1};
+  
+  }
+}
