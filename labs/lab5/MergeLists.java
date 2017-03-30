7214bbeb1d441aa7e2d89df461b7351f96324dae
Bob John <Bob John>
2017-03-30 01:31:14 -0400

    Implementation of a program that merges two user input arrays and sort least to greatest

--- labs/lab5/MergeLists.java
+++ labs/lab5/MergeLists.java
@@ -1,4 +1,4 @@
-//file: ConsecutiveFour.java
+//file: MergeLists.java
 //author: Shawn M Joseph
 //course: CMPT 220
 //assignment: lab 5
@@ -20,16 +20,30 @@ public class MergeLists {
     
 	
 	System.out.print("Enter the second list: ");
-	Scanner input2 = new Scanner(System.in);
 	int[] list2 = new int[input.nextInt()];
 	
-	for (int i = 0; i < list1.length; i++) {
-    list1[i] = input.nextInt();
+	for (int i = 0; i < list2.length; i++) {
+    list2[i] = input.nextInt();
     }
+	int [] list3 = merge (list1, list2);
+	System.out.print("The merged list: ");
+	
+	for (int i = 0; i < list3.length; i++) {
+	  System.out.print(list3[i] + " ");	
+	}
   }
   public static int[] merge(int[] list1, int[] list2) {
-	  
-	return int;
-    
+    int[] list3 = new int[list1.length + list2.length];
+    int s = 0, j = 0;
+    for (int i = 0; i < list3.length; i++) {
+      if (j < list2.length&&list1[s] > list2[j] ) {
+      list3[i] = list2[j];
+      j++;
+      } else if (s < list1.length) {
+      list3[i] = list1[s];
+      s++;
+      }
+    }
+	return list3;
   }
 }
