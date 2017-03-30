67456b5c14b9c173ed6673703d5c1bcf0414770c
Bob John <Bob John>
2017-03-29 21:46:42 -0400

    Made test program for method to merge two user inputted lists.

---
+++ labs/lab5/MergeLists.java
@@ -0,0 +1,35 @@
+//file: ConsecutiveFour.java
+//author: Shawn M Joseph
+//course: CMPT 220
+//assignment: lab 5
+//Due March 30th
+//version: 1.0
+
+import java.util.Scanner;
+
+public class MergeLists {
+  public static void main (String[] args) {
+	  
+	System.out.print("Enter the first list: ");  
+	Scanner input = new Scanner(System.in);  
+	int[] list1 = new int[input.nextInt()];
+	
+	for (int i = 0; i < list1.length; i++) {
+    list1[i] = input.nextInt();
+    }
+    
+	
+	System.out.print("Enter the second list: ");
+	Scanner input2 = new Scanner(System.in);
+	int[] list2 = new int[input.nextInt()];
+	
+	for (int i = 0; i < list1.length; i++) {
+    list1[i] = input.nextInt();
+    }
+  }
+  public static int[] merge(int[] list1, int[] list2) {
+	  
+	return int;
+    
+  }
+}
