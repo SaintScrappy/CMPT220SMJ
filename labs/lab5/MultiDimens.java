4103f4428dff622b8862ce439ab3d46a8cd2b1e8
Bob John <Bob John>
2017-04-09 02:49:45 -0400

    Completion of array programs for lab 5.

--- labs/lab5/MultiDimens.java
+++ labs/lab5/MultiDimens.java
@@ -12,13 +12,40 @@ public class MultiDimens {
 	  
 	System.out.print("Enter the rows and enter the columns: ");
     Scanner input = new Scanner(System.in);
+	
+	int s = input.nextInt();
+    int j = input.nextInt();
 	double[][] multilist = new double[s][j];
+	
 	System.out.print("Enter your array: ");
+	for (int x = 0; x < s; x++) {
+      for (int y = 0; y < j; y++) {
+        multilist[x][y] = input.nextDouble();
+	  }
+	}
+	
+	int[] result = locateLargest(multilist);
+	System.out.print("The largest element's location is ("+ result[0]+", "+result[1]+")");
   
   }
   public static int[] locateLargest(double[][] z) {
-	   for(int s=0; s< numbers.length; s++){
-         
-       }
+	double max = 0;
+	int[] largestint = new int [2];
+    for(int s=0; s < z.length; s++){
+	  for (int s2 = 0; s2 < z[s].length; s2++) {
+        if (z[s][s2] > max) {
+          max = z[s][s2];
+          largestint[0] = s;
+          largestint[1] = s2;
+        }
+      }
+    }
+	return largestint;    
   }
 }
+
+
+// 1 3 4 3 2 4 2 4 
+// 3 4 3 10 2 4 4 5
+
+//Column 4, Row 2 (4, 2)
