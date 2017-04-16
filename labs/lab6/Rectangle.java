58268cb66ac4572899e69f575b4da213d2cfaa67
Bob John <Bob John>
2017-04-16 16:35:04 -0400

    Begun creation of rectangle object, and DeathCalc object.

---
+++ labs/lab6/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+	
+  double width = 1;
+  double height = 1;
+  
+  Rectangle() {
+    
+  }
+  Rectangle() {
+	  
+  }
+ 
+  }
+}
---
+++ projects/2/DeathCalcTextGame/DeathCalculator.java
@@ -0,0 +1,26 @@
+//file: DeathCalculator.java
+//author: Shawn M Joseph
+//course: CMPT 220
+//assignment: Project 2
+//Due At The End.
+//version: 1.0
+
+import java.util.Scanner;
+
+public class DeathCalculator{
+  public static void main (String [] args){
+	  
+	Scanner input = new Scanner (System.in);  
+    System.out.println("You are now in DeathCalculator.exe.");
+	System.out.println("Introductory Paragraph*");
+	
+	String myHand = "No Item.";
+	String myHand2 = "Death Calculator";
+	String myOtherHand = "Compass";
+	
+	System.out.println("You currently have" + myHand + "in your hand.");
+	
+	int DeathCalc = 0;
+  
+  }
+}
