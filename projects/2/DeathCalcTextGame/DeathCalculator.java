eecd919133e13408a1f80f5de0499d170682dcf9
Bob John <Bob John>
2017-04-16 23:41:09 -0400

    Updates to work-in-progress text adventure game.

--- projects/2/DeathCalcTextGame/DeathCalculator.java
+++ projects/2/DeathCalcTextGame/DeathCalculator.java
@@ -12,15 +12,29 @@ public class DeathCalculator{
 	  
 	Scanner input = new Scanner (System.in);  
     System.out.println("You are now in DeathCalculator.exe.");
-	System.out.println("Introductory Paragraph*");
+	System.out.println("Welcome. You will be participating in a simulation known as the Death Calculator.");
+	System.out.println("**N/A");
+	System.out.println("**N/A");
 	
-	String myHand = "No Item.";
+	String userName;
+	System.out.println("What is your name?");
+	userName = input.nextLine();
+	System.out.println("Ah..." + userName + "That's a nice name..");
+	
+	String myHand = "No Item";
 	String myHand2 = "Death Calculator";
 	String myOtherHand = "Compass";
 	
 	System.out.println("You currently have" + myHand + "in your hand.");
 	
-	int DeathCalc = 0;
-  
+	DeathCalculator game = new DeathCalculator();
+	game.Calculation();
+	
   }
+  public void Calculation() {
+	  int DeathCalc = 0;
+	  if (DeathCalc >= 10)
+      System.exit(0);
+      System.out.println("The number on the calculator reads" + DeathCalc + ".");  
+    }
 }
