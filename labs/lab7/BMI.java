23d96f8c0ad84be682540fbea058b66de73d1417
Bob John <Bob John>
2017-04-28 16:14:36 -0400

    Finished addition to the BMI object.

--- labs/lab7/BMI.java
+++ labs/lab7/BMI.java
@@ -3,23 +3,33 @@ public class BMI {
   private String name;	
   private int age;
   private double weight;
+  private double height;
   private double feet;
   private double inches;
   public static final double KIL_PER_POUND = 0.45359237;
   public static final double MET_PER_INCH = 0.0254;
+  public static final double INCH_PER_FOOT = 12;
   
-  public BMI (String name, int age, double weight, double feet, double inches) {
+  public BMI(String name, int age, double weight, double height){
     this.name = name;
 	this.age = age;
 	this.weight = weight;
 	this.height = height;
   }
-  public getBMI() {
+  
+  public BMI(String name, double weight, double height){
+    this(name, 19, weight, height);
+  }
+  
+  public BMI (String name, int age, double weight, double feet, double inches) {
+    this(name, age, weight, inches + feet * 12);
+  }
+  public double getBMI() {
     double bmi = weight * KIL_PER_POUND / 
 	  ((height * MET_PER_INCH) * (height * MET_PER_INCH));
 	return Math.round(bmi * 100) / 100.0;
   }
-  public getStatus() {
+  public String getStatus() {
     double bmi = getBMI();
 	if (bmi < 18.5)
       return "Underweight";
