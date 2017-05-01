bbb37a9dc2a82e473ed445ccca299c86b31ed712
Bob John <Bob John>
2017-04-30 20:39:44 -0400

    Finished and implemented a program to get the first square numbers greater than Long.MAX_VALUE.

--- labs/lab7/SqLocate.java
+++ labs/lab7/SqLocate.java
@@ -1,6 +1,20 @@
+import java.math.BigDecimal;
+
 public class SqLocate {
-  public static void main (String[] args)
-  {
+  public static void main (String[] args){
   
+    BigDecimal longMaxValue = new BigDecimal(Long.MAX_VALUE);
+	long start = (long) Math.sqrt(Long.MAX_VALUE);
+	BigDecimal s = new BigDecimal(start);
+	int count = 0;
+	
+	while(count < 10){
+      BigDecimal squared = s.multiply(s);
+	  if(squared.compareTo(longMaxValue) > 0){
+	    count++;
+	    System.out.println(count+": " + s + "^2 = " + squared);	
+	  }
+	  s = s.add(BigDecimal.ONE);
+    }
   }
 }
