c3922b692db64fe718b7e1e065531b61405b502b
Bob John <Bob John>
2017-05-03 23:08:00 -0400

    Completion of lab 6 and progress towards lab 7.

--- labs/lab7/BMI.java
+++ labs/lab7/BMI.java
@@ -43,14 +43,26 @@ public class BMI {
   public String getName(){
     return name;
   }
+  public void setName(String name){
+	this.name = name;
+  }
   public int getAge(){
 	return age;
+  }
+  public void setAge(int age){
+	this.age = age;
   }
   public double getWeight(){
 	return weight;
   }
+  public void setWeight(double weight){
+	this.weight = weight;
+  }
   public double getHeight(){
 	return height;
+  }
+  public void setHeight(double height){
+	this.height = height;
   }
   public double getInches(){
 	return inches;
--- labs/lab7/Queue.java
+++ labs/lab7/Queue.java
@@ -1,15 +1,35 @@
 public class Queue {
+	
   private int[] elements;
   private int size;
+  private int capacity = 8;
+  
+  
   private Queue () {
-  //capacity = 8
+    
   }
   public static enqueue(int v) {
+  //adds v into the queue
   }
   public static dequeue() {
+  //removes return element from queue
   }
   public static empty () {
+  //return true if queue is empty (loop)
   }
   public static getSize() {
+  //return queue size
+  }
+  public int[] getElements() {
+    return elements;
+  }
+  public void setElements(int[] elements){
+    this.elements = elements;
+  }
+  public int getSizeize() {
+	return size;
+  }
+  public void setSize {
+    this.
   }
 }
